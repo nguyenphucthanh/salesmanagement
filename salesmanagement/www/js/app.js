395a6abd3942576df21321ba43bab8ddc93d3abe
thanhnguyen <nguyenphucthanh@live.com>
2016-03-28 23:44:43 +0700

    update

--- salesmanagement/www/js/app.js
+++ salesmanagement/www/js/app.js
@@ -4,70 +4,58 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform, $localStorage, $state) {
+    $ionicPlatform.ready(function () {
+      try {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
+      catch(ex) {
+        console.error(ex);
       }
-    })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+      if(!$localStorage.userLogin) {
+        $state.go('login');
       }
-    }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $stateProvider
+
+      .state('home', {
+        url: '/',
+        abstract: true,
+        templateUrl: 'templates/home.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/');
+
+    // Disable cache
+    //$ionicConfigProvider.views.maxCache(0);
+  })
+
+  .constant('CONFIG', {
+    server: {
+      'local': 'http://visitme.cloudapp.net:83/Home/',
+      'live': 'http://antvn.vn/'
+    }['local']
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
