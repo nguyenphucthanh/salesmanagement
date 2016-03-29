0a9d16a3a2947e3ac0e54847738f320b75757ece
thanhnguyen <nguyenphucthanh@live.com>
2016-03-29 19:19:02 +0700

    update

--- salesmanagement/www/js/Services/ReportService.js
+++ salesmanagement/www/js/Services/ReportService.js
@@ -13,9 +13,14 @@ angular.module('starter')
         "Password": password,
         "Imei": imei
       }).then(function (res) {
-
+        if(res.ResultCode === 'Success') {
+          deferred.resolve(res.Result);
+        }
+        else {
+          deferred.reject(res.ResultMessages);
+        }
       }, function (error) {
-
+        deferred.reject(error);
       });
 
       return deferred.promise;
